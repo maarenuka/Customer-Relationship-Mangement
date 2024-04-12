dfcdcb978123a2000046029cc64dcfe790868503
Perabathula Maa Renuka <131272572+maarenuka@users.noreply.github.com>
2024-04-12 16:42:08 +0530

    Update models.py

--- customers/models.py
+++ customers/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-# Create your models here.
-
 class Customer(models.Model):
     first_name = models.CharField("First name", max_length=255)
     last_name = models.CharField("Last name", max_length=255)
